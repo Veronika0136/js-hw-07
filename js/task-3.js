ad6eb1221cf74f30a54633c591df72780e36196b
Veronika0136 <Artemova331@gmail.com>
2025-02-09 15:00:22 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,13 +2,9 @@ const onInput = document.querySelector('input#name-input');
 const onSpan = document.querySelector('span#name-output');
 
 onInput.addEventListener('input', e => {
-  console.log(e);
-  e.target.textContent.trim();
- 
-  if(e.target.value.includes(" ") || e.target.value === ""){
-    onSpan.textContent = "Anonymous";
-  } else {
-    onSpan.textContent = e.target.value;
+  onSpan.textContent = e.target.value.trim();
+
+  if (e.target.value.trim() === '' || e.target.value.trim() === ' ') {
+    onSpan.textContent = 'Anonymous';
   }
-  
 });
