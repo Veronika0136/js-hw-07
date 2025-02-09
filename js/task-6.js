e1acfc4b59c29441d8a1be199be3257847894aaf
Veronika0136 <Artemova331@gmail.com>
2025-02-09 22:46:50 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+let color = getRandomHexColor();
+
+btnCreate.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Введіть число від 1 до 100!');
+  }
+});
+
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = color;
+    boxes.appendChild(box);
+  }
+}
+
