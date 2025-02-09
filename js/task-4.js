204f841d5b87665075e6f2e52b829e584c85f710
Veronika0136 <Artemova331@gmail.com>
2025-02-09 16:45:50 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('form.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(evt);
+
+  const email = evt.target.elements.email.value.trim();
+  console.log(email);
+
+  const password = evt.target.elements.password.value.trim();
+  console.log(password);
+
+  const data = {
+    email: email,
+    password: password,
+  };
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  } else {
+    console.log(data);
+  }
+
+  form.reset();
+}
